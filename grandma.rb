f91ce50bcde9ebdb9ccb477860eaf8db6f915765
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-06 23:29:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
