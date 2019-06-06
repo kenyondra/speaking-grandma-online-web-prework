038a8b7bbce4a0c641785c3e1785a1bed937e1d5
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-06 23:28:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   
-  if phrase == "I LOVE YOU"
+  if phrase == "I LOVE YOU GRANDMA"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
