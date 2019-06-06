106cf2f6ccf983fbcb1f93bed1a9b6082dbe6482
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-06 23:34:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
     
   elsif phrase == phrase.downcase
    return "HUH?! SPEAK UP, SONNY!"
-   
+  elsif  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
