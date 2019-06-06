4e43898a10fe15ac74a92ceca680a40529076209
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-06 23:38:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,6 +10,8 @@ def speak_to_grandma(phrase)
    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
    return "NO, NOT SINCE 1938"
+   
+  else 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
